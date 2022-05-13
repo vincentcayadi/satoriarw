37912776aacecf19fb951ae2bb4cd88ea3eca585
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-05-12 20:40:45 -0700

    Added Experience

--- components/Nav.js
+++ components/Nav.js
@@ -3,24 +3,29 @@ import styles from "../styles/Nav.module.css";
 
 const Nav = () => {
   return (
-    <nav className="mx-auto text-white bg-transparent">
+    <nav className="mx-auto bg-transparent text-sky-50 ">
       <div className="flex justify-evenly">
-        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-white">
+        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-sky-50">
           <Link href="/">
             <a>Home</a>
           </Link>
         </div>
-        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-white">
+        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-sky-50">
           <Link href="/blog">
             <a>Blog</a>
           </Link>
         </div>
-        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-white">
+        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-sky-50">
+          <Link href="/experience">
+            <a>Experience</a>
+          </Link>
+        </div>
+        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-sky-50">
           <Link href="/projects">
             <a>Projects</a>
           </Link>
         </div>
-        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-white">
+        <div className="text-xl duration-300 border-b-2 border-b-transparent hover:border-b-2 hover:border-b-sky-50">
           <Link href="/contact">
             <a>Contact</a>
           </Link>
---
+++ pages/experience.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Nav";
+
+export default function Experience() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1 className="m-2 text-xl text-white">Experience</h1>
+      </div>
+    </>
+  );
+}
