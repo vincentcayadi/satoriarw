0319aaf637152146191973d7c0661e1a576846ce
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-05-19 17:24:02 -0700

    added content to experinece.js

--- components/RoboCard.js
+++ components/RoboCard.js
@@ -11,9 +11,7 @@ const RoboCard = () => {
               <div className="px-6 py-4 rounded-bl-md">
                 <div className="mb-2 text-xl font-bold ">Autodesk AutoCAD</div>
                 <p className="text-base ">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
+                  Used AutoCAD for generating 2D and 3D models for my robots.
                 </p>
               </div>
             </div>
@@ -21,9 +19,7 @@ const RoboCard = () => {
               <div className="px-6 py-4 rounded-br-md">
                 <div className="mb-2 text-xl font-bold">Fusion 360</div>
                 <p className="text-base ">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
+                  Moved to Fusion 360 as it is more powerful and has a Altium integration.
                 </p>
               </div>
             </div>
@@ -35,31 +31,16 @@ const RoboCard = () => {
             <div className="max-w-sm overflow-hidden">
               <div className="px-6 py-4 rounded-bl-md">
                 <div className="mb-2 text-xl font-bold">Altium</div>
-                <p className="text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
-                </p>
               </div>
             </div>
             <div className="max-w-sm overflow-hidden">
               <div className="px-6 py-4 rounded-br-md">
                 <div className="mb-2 text-xl font-bold">KiCad</div>
-                <p className="text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
-                </p>
               </div>
             </div>
             <div className="max-w-sm overflow-hidden">
               <div className="px-6 py-4 rounded-br-md">
                 <div className="mb-2 text-xl font-bold">Eagle</div>
-                <p className="text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
-                </p>
               </div>
             </div>
           </div>
--- components/WebDevCard.js
+++ components/WebDevCard.js
@@ -6,28 +6,18 @@ const WebDevCard = () => {
     <>
       <Accordion>
         <Accordion.Item label="Front End">
-          <div className="grid grid-cols-2 gap-6 bg-opacity-20">
+          <div className="grid grid-cols-2 gap-6">
             <div className="max-w-sm overflow-hidden">
-              <div className="px-6 py-4 bg-opacity-20 rounded-bl-md">
-                <div className="mb-2 text-xl font-bold bg-transparent">
-                  React.js
-                </div>
-                <p className="text-base bg-transparent">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
-                </p>
+              <div className="px-6 py-4 ">
+                <div className="mb-2 text-xl font-bold ">React.js</div>
+                <p className="text-base ">Used React for BlockVault</p>
               </div>
             </div>
             <div className="max-w-sm overflow-hidden">
-              <div className="px-6 py-4 bg-opacity-20 rounded-br-md">
-                <div className="mb-2 text-xl font-bold bg-transparent">
-                  Next.js
-                </div>
-                <p className="text-base bg-transparent">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
+              <div className="px-6 py-4 ">
+                <div className="mb-2 text-xl font-bold ">Next.js</div>
+                <p className="text-base ">
+                  Built this website using Next.js and Tailwind and Mantine
                 </p>
               </div>
             </div>
@@ -35,25 +25,19 @@ const WebDevCard = () => {
         </Accordion.Item>
 
         <Accordion.Item label="Back End">
-          <div className="grid grid-cols-2 gap-6 bg-opacity-20">
+          <div className="grid grid-cols-2 gap-6">
             <div className="max-w-sm overflow-hidden">
-              <div className="px-6 py-4 bg-opacity-20 rounded-bl-md">
-                <div className="mb-2 text-xl font-bold bg-transparent">
-                  MongoDB
-                </div>
-                <p className="text-base bg-transparent">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,x
-                  exercitationem praesentium nihil.
+              <div className="px-6 py-4 ">
+                <div className="mb-2 text-xl font-bold ">MongoDB</div>
+                <p className="text-base ">
+                  Used MongoDB for my ecommerce website
                 </p>
               </div>
             </div>
             <div className="max-w-sm overflow-hidden">
-              <div className="px-6 py-4 bg-opacity-20 rounded-br-md">
-                <div className="mb-2 text-xl font-bold bg-transparent">
-                  Firebase
-                </div>
-                <p className="text-base bg-transparent">
+              <div className="px-6 py-4 ">
+                <div className="mb-2 text-xl font-bold ">Firebase</div>
+                <p className="text-base ">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Voluptatibus quia, nulla! Maiores et perferendis eaque,x
                   exercitationem praesentium nihil.
