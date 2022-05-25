22eb2171b95e68c8952b3c063964d59523064c3a
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-05-25 00:24:35 -0700

    added icons

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
 import Accordion from "../components/Accordion";
-
+import { IconContext } from "react-icons";
+import { FaDiscord } from "react-icons/fa";
+import { FiCoffee } from "react-icons/fi";
+import { SiKofi } from "react-icons/si";
+import { VscGithubAlt } from "react-icons/vsc";
 export default function Home() {
   return (
     <>
       <div className="w-1/2 mx-auto">
-        <div className="grid h-screen place-items-center">
+        <div className="grid h-screen place-items-center" id="#">
           <div>
             <div className="inline-block mr-12 align-top">
               <h1 className="text-3xl font-bold text-slate-100">
@@ -22,11 +25,6 @@ export default function Home() {
                 </Link>
               </p>
               <p className="mt-2 text-xl duration-300 text-slate-400 hover:translate-x-2">
-                <Link href="#contact">
-                  <a>Contact Me →</a>
-                </Link>
-              </p>
-              <p className="mt-2 text-xl duration-300 text-slate-400 hover:translate-x-2">
                 <Link href="#projects">
                   <a>My Projects →</a>
                 </Link>
@@ -37,30 +35,24 @@ export default function Home() {
                 </Link>
               </p>
             </div>
-            <div className="inline-block align-top">
-              <Image
-                src="https://avatars.githubusercontent.com/u/57314503?v=4"
-                width={240}
-                height={240}
-                alt="pfp"
-                className="rounded-2xl"
-              ></Image>
+            <div className="grid w-1/3 grid-cols-3 my-4 auto-rows-fr">
+              <IconContext.Provider value={{ size: "1.5rem" }}>
+                <span className="inline-block p-2 mx-auto duration-200 rounded-xl bg-slate-700 hover:scale-110">
+                  <FaDiscord />
+                </span>
+                <span className="inline-block p-2 mx-auto duration-200 rounded-xl bg-slate-700 hover:scale-110">
+                  <VscGithubAlt />
+                </span>
+                <span className="inline-block p-2 mx-auto duration-200 rounded-xl bg-slate-700 hover:scale-110">
+                  <FiCoffee />
+                </span>
+              </IconContext.Provider>
             </div>
           </div>
         </div>
-        <br></br>
-        <div id="contact" className="h-screen pt-10">
-          <h1>Contact Me</h1>
-          <div className="w-full mt-10 rounded-xl h-1/2 bg-slate-500">
-            <div className="grid h-full place-items-center">
-              <div className="grid bg-black rounded-full h-28 w-28 place-items-center"></div>
-            </div>
-          </div>
-        </div>
-        <br></br>
         <div id="projects" className="h-screen pt-10">
           <h1>Projects</h1>
-          <div className="grid grid-cols-3 gap-6 mt-10 h-80 auto-cols-max auto-rows-max">
+          <div className="grid grid-cols-3 gap-6 mt-10 h-80 auto-rows-max">
             <div className="p-2 duration-200 border-2 rounded-xl hover:scale-110">
               <div className="max-w-sm overflow-hidden">
                 <div className="px-6 py-4">
@@ -109,7 +101,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <br></br>
         <div id="skill" className="h-screen pt-10">
           <div>
             <h1>My Skills</h1>
