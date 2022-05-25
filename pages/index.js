25b150f77872760e85a1a1670ac05b1a3d6132cd
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-05-25 01:23:26 -0700

    Added Icons And ToolTip

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 import Accordion from "../components/Accordion";
+import { Tooltip } from "@mantine/core";
 import { IconContext } from "react-icons";
 import { FaDiscord } from "react-icons/fa";
 import { FiCoffee } from "react-icons/fi";
-import { SiKofi } from "react-icons/si";
 import { VscGithubAlt } from "react-icons/vsc";
 export default function Home() {
   return (
     <>
       <div className="w-1/2 mx-auto">
-        <div className="grid h-screen place-items-center" id="#">
+        <div className="grid h-screen place-items-center">
           <div>
             <div className="inline-block mr-12 align-top">
               <h1 className="text-3xl font-bold text-slate-100">
@@ -34,19 +35,60 @@ export default function Home() {
                   <a>My Skills →</a>
                 </Link>
               </p>
+              <div className="grid w-1/3 grid-cols-3 gap-2 my-4 auto-rows-fr">
+                <IconContext.Provider value={{ size: "1.5rem" }}>
+                  <Tooltip
+                    color="dark"
+                    withArrow
+                    position="left"
+                    transition="fade"
+                    transitionDuration={200}
+                    label="Come chat with me on Discord"
+                    className="inline-block p-2 mx-auto duration-200 rounded-lg shadow-xl bg-slate-800 hover:scale-105"
+                  >
+                    <Link href="https://discord.com/users/678790092000854017">
+                      <a>
+                        <FaDiscord />
+                      </a>
+                    </Link>
+                  </Tooltip>
+                  <Tooltip
+                    color="dark"
+                    withArrow
+                    position="bottom"
+                    transition="fade"
+                    transitionDuration={200}
+                    label="View My projects on Github"
+                    className="inline-block p-2 mx-auto duration-200 rounded-lg shadow-xl bg-slate-800 hover:scale-105"
+                  >
+                    <Link href="https://github.com/Vincent-Cayadi">
+                      <a>
+                        <VscGithubAlt />
+                      </a>
+                    </Link>
+                  </Tooltip>
+                  <Tooltip
+                    color="dark"
+                    withArrow
+                    position="right"
+                    transition="fade"
+                    transitionDuration={200}
+                    label="Support Me!"
+                    className="inline-block p-2 mx-auto duration-200 rounded-lg shadow-xl bg-slate-800 hover:scale-105"
+                  >
+                    <FiCoffee />
+                  </Tooltip>
+                </IconContext.Provider>
+              </div>
             </div>
-            <div className="grid w-1/3 grid-cols-3 my-4 auto-rows-fr">
-              <IconContext.Provider value={{ size: "1.5rem" }}>
-                <span className="inline-block p-2 mx-auto duration-200 rounded-xl bg-slate-700 hover:scale-110">
-                  <FaDiscord />
-                </span>
-                <span className="inline-block p-2 mx-auto duration-200 rounded-xl bg-slate-700 hover:scale-110">
-                  <VscGithubAlt />
-                </span>
-                <span className="inline-block p-2 mx-auto duration-200 rounded-xl bg-slate-700 hover:scale-110">
-                  <FiCoffee />
-                </span>
-              </IconContext.Provider>
+            <div className="inline-block align-top">
+              <Image
+                src="https://avatars.githubusercontent.com/u/57314503?v=4"
+                width={240}
+                height={240}
+                alt="pfp"
+                className="rounded-2xl"
+              ></Image>
             </div>
           </div>
         </div>
