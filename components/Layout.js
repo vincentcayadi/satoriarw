607927f231ffb351f837c31ccd7caae9d872417b
Vincent Cayadi <57314503+Vincent-Cayadi@users.noreply.github.com>
2022-08-13 12:17:02 +0800

    Update meta tag

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
           <title>Vincent Cayadi</title>
           <meta
             name="name"
-            content="web development, programming, student, athlete, swimmer, github, discord, developer, learner, engineer, mechanical engineer, mechanical engineering, electrical, electrical engineer, electrical engineering"
+            content="vincent,vincentcayadi,cayadi,cayadivincent,web development, programming, student, athlete, swimmer, github, discord, developer, learner, engineer, mechanical engineer, mechanical engineering, electrical, electrical engineer, electrical engineering"
           ></meta>
           <link
             href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
