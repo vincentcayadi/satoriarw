f45d3927f73306aa83a3e35ed8e40e76eaad3598
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-17 18:39:21 +0800

    Done with blog needa do abit mroe work on styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "floral-white": "#fbf8f3",
+        nero: "#232323",
+      },
+    },
     fontFamily: {
       sans: ["Manrope", "sans-serif"],
     },
