283a4ef4e24c37d77995570ce21471aa2e905dd6
Vincent Cayadi <cayadivincent@gmail.com>
2024-06-21 01:52:57 +0800

    seocnd time

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "react-feather";
 
 export default function Carousel({ children: slides }) {
