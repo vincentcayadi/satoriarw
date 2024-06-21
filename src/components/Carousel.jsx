958fa2825616b23e5ac9b6bdb524c8323d49b9ba
Vincent Cayadi <cayadivincent@gmail.com>
2024-06-22 04:47:06 +0800

    pain

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -29,10 +29,10 @@ export default function Carousel({ children: slides }) {
         {slides}
       </div>
       <div className="absolute inset-0 flex items-center justify-between px-4 md:px-8">
-        <button onClick={prevSlide} className="z-40">
+        <button onClick={prevSlide} className="z-40" aria-label="Previous Slide">
           <ArrowLeftCircle className="w-4 h-4 duration-200 md:w-10 md:h-10 hover:scale-110 stroke-neutral-200/80" />
         </button>
-        <button onClick={nextSlide} className="z-40">
+        <button onClick={nextSlide} className="z-40" aria-label="Next Slide">
           <ArrowRightCircle className="w-4 h-4 duration-200 md:w-10 md:h-10 hover:scale-110 stroke-neutral-200/80" />
         </button>
       </div>
