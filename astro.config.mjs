043c7edab9ef830ebeae688f9573c2c28f683343
Vincent Cayadi <cayadivincent@gmail.com>
2024-06-23 04:00:36 +0800

    Revert "gen og images testing"
    This reverts commit ab0ffe912f4ede0705b129843c6c1bd18486186f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://satoriarw.vercel.app/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -13,6 +11,5 @@ export default defineConfig({
     react({
       experimentalReactChildren: true,
     }),
-    sitemap(),
   ],
 });
