c992ee15e1ea751a7cd108d3c343ed767c67bbd7
Vincent Cayadi <cayadivincent@gmail.com>
2024-06-23 03:54:35 +0800

    cry v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://satoriarw.vercel.app/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -11,5 +13,6 @@ export default defineConfig({
     react({
       experimentalReactChildren: true,
     }),
+    sitemap(),
   ],
 });
---
+++ src/data/socials.ts
@@ -0,0 +1,11 @@
+export const author = "Sentinels Robotics";
+
+export const defaultMeta = {
+  title: "Sentinels Robotics",
+  description: "The best way to learn is to watch your robot go up in flames.",
+  ogImage: {
+    src: "/images/banner.webp",
+    alt: "Sentinels Robotics",
+  },
+  wesbite: "https://senrobo.github.io",
+};
