b2310b0bca2311fc03f42eb04e6f4d1b3eca8a0f
Vincent Cayadi <cayadivincent@gmail.com>
2024-06-23 04:07:48 +0800

    Revert "work?"
    This reverts commit d056c8fbd84fc4719aa46639cab1b0eaa43c143f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import satoriAstro from "satori-astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +11,5 @@ export default defineConfig({
     react({
       experimentalReactChildren: true,
     }),
-    satoriAstro(),
   ],
 });
